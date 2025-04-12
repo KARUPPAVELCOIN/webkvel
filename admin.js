6a22b4806078ff23a87f434f66accabf2181f064
KARUPPAVELCOIN <karuppavelcoin@gmail.com>
2025-04-12 23:36:46 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -32,7 +32,7 @@ async function sendMessage() {
     };
 
     try {
-        const response = await fetch('http://localhost:3000/messages', {
+        const response = await fetch('https://github.com/KARUPPAVELCOIN/webkvel/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(newMessage)
